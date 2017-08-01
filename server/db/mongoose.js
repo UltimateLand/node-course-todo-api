0740d4a8a7fa5dd8be6dfe45da1eb13d94e1f8ba
Jun-Ren Chen <Jun-Ren, Chen>
2017-08-02 00:40:07 +0800

    Add DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://Horace:g411314learn@ds127783.mlab.com:27783/to-do-api' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://Horace:g411314learn@ds127783.mlab.com:27783/to-do-api' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};

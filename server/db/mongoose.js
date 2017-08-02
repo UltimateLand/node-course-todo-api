aab77328a4a9a2b9295414f92c3f308be6af8c57
Jun-Ren Chen <Jun-Ren, Chen>
2017-08-02 17:21:29 +0800

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost:27017/TodoApp'); //for local test
-mongoose.connect('mongodb://Horace:g411314learn@ds127783.mlab.com:27783/to-do-api' || 'mongodb://localhost:27017/TodoApp'); //for deploy on heroku
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
